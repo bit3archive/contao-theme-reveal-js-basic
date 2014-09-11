4ac5bcdb457f15555dd1ba647d74c38dff924fe6
Tristan Lins <tristan.lins@bit3.de>
2014-09-11 22:59:53 +0200

    Only show slide numbers on reveal pages.

--- src/Hooks.php
+++ src/Hooks.php
@@ -142,89 +142,94 @@ class Hooks
 
     public function getArticleLabel($row, $label)
     {
+        $page   = \PageModel::findWithDetails($row['pid']);
+        $layout = $page->getRelated('layout');
+
         $callback = $GLOBALS['TL_DCA']['tl_article']['list']['label']['reveal_original_label_callback'];
         if (is_array($callback)) {
             $callback[0] = \System::importStatic($callback[0]);
         }
         $label = call_user_func($callback, $row, $label);
 
-        if ($row['revealVerticalSlide'] == 'start') {
-            $label = '&boxhd; ' . $label;
-        } else {
-            if ($row['revealVerticalSlide'] == 'stop') {
-                $label = '&boxhu; ' . $label;
+        if ($layout->useRevealJs) {
+            if ($row['revealVerticalSlide'] == 'start') {
+                $label = '&boxhd; ' . $label;
             } else {
-                $predecessors = \ArticleModel::findBy(
-                    array('pid = ?', 'sorting < ?', 'revealVerticalSlide != ?'),
-                    array($row['pid'], $row['sorting'], ''),
-                    array('order' => 'sorting DESC', 'limit' => 1)
-                );
-
-                if ($predecessors && $predecessors->revealVerticalSlide == 'start') {
-                    $successor = \ArticleModel::findOneBy(
-                        array('pid = ?', 'sorting > ?'),
-                        array($row['pid'], $row['sorting']),
-                        array('order' => 'sorting', 'limit' => 1)
+                if ($row['revealVerticalSlide'] == 'stop') {
+                    $label = '&boxhu; ' . $label;
+                } else {
+                    $predecessors = \ArticleModel::findBy(
+                        array('pid = ?', 'sorting < ?', 'revealVerticalSlide != ?'),
+                        array($row['pid'], $row['sorting'], ''),
+                        array('order' => 'sorting DESC', 'limit' => 1)
                     );
 
-                    if ($successor && $successor->revealVerticalSlide == 'start') {
-                        $label = '&boxhu; ' . $label;
-                    } else {
-                        $label = '&boxv; ' . $label;
+                    if ($predecessors && $predecessors->revealVerticalSlide == 'start') {
+                        $successor = \ArticleModel::findOneBy(
+                            array('pid = ?', 'sorting > ?'),
+                            array($row['pid'], $row['sorting']),
+                            array('order' => 'sorting', 'limit' => 1)
+                        );
+
+                        if ($successor && $successor->revealVerticalSlide == 'start') {
+                            $label = '&boxhu; ' . $label;
+                        } else {
+                            $label = '&boxv; ' . $label;
+                        }
                     }
                 }
             }
-        }
 
-        $predecessors = \ArticleModel::findBy(
-            array('pid = ?', 'sorting < ?'),
-            array($row['pid'], $row['sorting']),
-            array('order' => 'sorting')
-        );
+            $predecessors = \ArticleModel::findBy(
+                array('pid = ?', 'sorting < ?'),
+                array($row['pid'], $row['sorting']),
+                array('order' => 'sorting')
+            );
 
-        if ($predecessors) {
-            $slide = $predecessors->count();
-            $page  = -1;
+            if ($predecessors) {
+                $slide = $predecessors->count();
+                $page  = -1;
 
-            $inVertical = false;
-            foreach ($predecessors as $predecessor) {
-                if ($predecessor->revealVerticalSlide == 'start') {
-                    $inVertical = true;
+                $inVertical = false;
+                foreach ($predecessors as $predecessor) {
+                    if ($predecessor->revealVerticalSlide == 'start') {
+                        $inVertical = true;
+                    }
+
+                    if ($inVertical && $predecessor->revealVerticalSlide != 'start') {
+                        $page += .001;
+                    } else {
+                        $page = (int) ($page + 1);
+                    }
+
+                    if ($predecessor->revealVerticalSlide == 'stop') {
+                        $inVertical = false;
+                    }
                 }
 
-                if ($inVertical && $predecessor->revealVerticalSlide != 'start') {
+                if ($inVertical && $row['revealVerticalSlide'] != 'start') {
                     $page += .001;
                 } else {
                     $page = (int) ($page + 1);
                 }
 
-                if ($predecessor->revealVerticalSlide == 'stop') {
-                    $inVertical = false;
+                if ($inVertical) {
+                    $pageMain = (int) $page;
+                    $pageSub  = (int) (($page - $pageMain) * 1000);
+
+                    $page = sprintf('%d-%d', $pageMain, $pageSub);
+                } else {
+                    $page = (int) $page;
                 }
-            }
 
-            if ($inVertical && $row['revealVerticalSlide'] != 'start') {
-                $page += .001;
             } else {
-                $page = (int) ($page + 1);
+                $slide = 0;
+                $page  = 0;
             }
 
-            if ($inVertical) {
-                $pageMain = (int) $page;
-                $pageSub  = (int) (($page - $pageMain) * 1000);
-
-                $page = sprintf('%d-%d', $pageMain, $pageSub);
-            } else {
-                $page = (int) $page;
-            }
-
-        } else {
-            $slide = 0;
-            $page  = 0;
+            $label .= ' ' . sprintf($GLOBALS['TL_LANG']['tl_article']['revealSlideNumber'], $slide, $page);
         }
-
-        $label .= ' ' . sprintf($GLOBALS['TL_LANG']['tl_article']['revealSlideNumber'], $slide, $page);
-
+        
         return $label;
     }
 
